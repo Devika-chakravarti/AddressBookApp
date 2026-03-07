5691a12f3b5ff723b517b5ec539cdcbca676e13f
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-03-07 15:21:39 +0530

    [Devika] : Modified AddressBook file

--- src/main/java/com/addressbook/service/AddressBook.java
+++ src/main/java/com/addressbook/service/AddressBook.java
@@ -1,38 +1,50 @@
 package com.addressbook.service;
 
+import java.util.ArrayList;
+
 import com.addressbook.model.ContactPerson;
 
 public class AddressBook {
-	private ContactPerson person;
+	private ArrayList<ContactPerson> personList = new ArrayList<>();
 
 	public void addContact(ContactPerson person) {
-		this.person = person;
+		personList.add(person);
 	}
 
 	public boolean editContactByName(String firstName, String lastName, String address, String city, String state,
 			String zip, String phoneNumber, String email) {
 
-		if (person != null && person.getFirstName().equalsIgnoreCase(firstName)) {
-			person.updateDetails(lastName, address, city, state, zip, phoneNumber, email);
-			return true;
+		for (ContactPerson person : personList) {
+			if (person.getFirstName().equalsIgnoreCase(firstName)) {
+				person.updateDetails(lastName, address, city, state, zip, phoneNumber, email);
+				return true;
+			}
 		}
 		return false;
 	}
 
 	public boolean deleteContactByName(String firstName) {
-		if (person != null && person.getFirstName().equalsIgnoreCase(firstName)) {
-			person = null;
-			return true;
+		for (int i = 0; i < personList.size(); i++) {
+			if (personList.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+				personList.remove(i);
+				return true;
+			}
 		}
 		return false;
 	}
 
 	@Override
 	public String toString() {
-		if (person == null) {
+		if (personList.isEmpty()) {
 			return "ADDRESS BOOK IS EMPTY";
 		}
 
-		return "ADDRESS BOOK CONTACT DETAILS\n" + "----------------------------\n" + person;
+		String result = "ADDRESS BOOK CONTACT DETAILS\n----------------------------\n";
+
+		for (ContactPerson person : personList) {
+			result += person + "\n----------------------------\n";
+		}
+
+		return result;
 	}
 }
