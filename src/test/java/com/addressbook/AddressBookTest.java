a81bf508945d1acb67ed3934c47aabd5f9cda613
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-03-07 14:18:10 +0530

    [Devika] : Modified AddressBookTest file

--- src/test/java/com/addressbook/AddressBookTest.java
+++ src/test/java/com/addressbook/AddressBookTest.java
@@ -1,6 +1,7 @@
 package com.addressbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,18 +11,39 @@ import com.addressbook.service.AddressBook;
 public class AddressBookTest {
 
 	@Test
-	void givenContact_WhenAddedToAddressBook_ShouldReturnExpectedAddressBookDetails() {
+	void givenContact_WhenAddedToAddressBook_ShouldReturnExpectedDetails() {
 		ContactPerson person = new ContactPerson("Devika", "Chakravarti", "Shivajinagar", "Katni", "Madhya Pradesh",
 				"483501", "9876580098", "devika@example.com");
 
 		AddressBook addressBook = new AddressBook();
 		addressBook.addContact(person);
 
-		String expectedOutput = "ADDRESS BOOK CONTACT DETAILS\n" + "----------------------------\n"
+		String expected = "ADDRESS BOOK CONTACT DETAILS\n" + "----------------------------\n"
 				+ "First Name   : Devika\n" + "Last Name    : Chakravarti\n" + "Address      : Shivajinagar\n"
 				+ "City         : Katni\n" + "State        : Madhya Pradesh\n" + "Zip          : 483501\n"
 				+ "Phone Number : 9876580098\n" + "Email        : devika@example.com";
 
-		assertEquals(expectedOutput, addressBook.toString());
+		assertEquals(expected, addressBook.toString());
+	}
+
+	@Test
+	void givenExistingContactName_WhenEdited_ShouldUpdateContactSuccessfully() {
+		ContactPerson person = new ContactPerson("Devika", "Chakravarti", "Shivajinagar", "Katni", "Madhya Pradesh",
+				"483501", "9876580098", "devika@example.com");
+
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact(person);
+
+		boolean result = addressBook.editContactByName("Devika", "Sharma", "Vijay Nagar", "Indore", "Madhya Pradesh",
+				"452010", "9999999999", "devika.sharma@example.com");
+
+		assertTrue(result);
+
+		String expected = "ADDRESS BOOK CONTACT DETAILS\n" + "----------------------------\n"
+				+ "First Name   : Devika\n" + "Last Name    : Sharma\n" + "Address      : Vijay Nagar\n"
+				+ "City         : Indore\n" + "State        : Madhya Pradesh\n" + "Zip          : 452010\n"
+				+ "Phone Number : 9999999999\n" + "Email        : devika.sharma@example.com";
+
+		assertEquals(expected, addressBook.toString());
 	}
 }
