2841db54ddeadf0bad9cc0d216a5a30c2485d553
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-03-09 00:22:14 +0530

    [Devika] : Modified AddressBookSystem file

--- src/main/java/com/addressbook/service/AddressBookSystem.java
+++ src/main/java/com/addressbook/service/AddressBookSystem.java
@@ -1,6 +1,10 @@
 package com.addressbook.service;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.addressbook.model.ContactPerson;
 
 public class AddressBookSystem {
 	private HashMap<String, AddressBook> addressBookMap = new HashMap<>();
@@ -16,6 +20,16 @@ public class AddressBookSystem {
 
 	public AddressBook getAddressBook(String addressBookName) {
 		return addressBookMap.get(addressBookName);
+	}
+
+	public List<ContactPerson> searchPersonByCity(String city) {
+		return addressBookMap.values().stream().flatMap(addressBook -> addressBook.getPersonsByCity(city).stream())
+				.collect(Collectors.toList());
+	}
+
+	public List<ContactPerson> searchPersonByState(String state) {
+		return addressBookMap.values().stream().flatMap(addressBook -> addressBook.getPersonsByState(state).stream())
+				.collect(Collectors.toList());
 	}
 
 	@Override
