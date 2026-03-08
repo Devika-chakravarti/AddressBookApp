1ace1eedf4e4731a71bf06934704194ca8cff385
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-03-09 00:23:47 +0530

    [Devika] : Modified ContactTest file

--- src/test/java/com/addressbook/ContactTest.java
+++ src/test/java/com/addressbook/ContactTest.java
@@ -1,6 +1,7 @@
 package com.addressbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,10 +14,30 @@ public class ContactTest {
 		ContactPerson person = new ContactPerson("Devika", "Chakravarti", "Shivajinagar", "Katni", "Madhya Pradesh",
 				"483501", "9876580098", "devika@example.com");
 
-		String expectedOutput = "First Name   : Devika\n" + "Last Name    : Chakravarti\n"
-				+ "Address      : Shivajinagar\n" + "City         : Katni\n" + "State        : Madhya Pradesh\n"
-				+ "Zip          : 483501\n" + "Phone Number : 9876580098\n" + "Email        : devika@example.com";
+		String expected = "First Name   : Devika\n" + "Last Name    : Chakravarti\n" + "Address      : Shivajinagar\n"
+				+ "City         : Katni\n" + "State        : Madhya Pradesh\n" + "Zip          : 483501\n"
+				+ "Phone Number : 9876580098\n" + "Email        : devika@example.com";
 
-		assertEquals(expectedOutput, person.toString());
+		assertEquals(expected, person.toString());
+	}
+
+	@Test
+	void givenSameNameContacts_WhenCompared_ShouldBeEqual() {
+		ContactPerson person1 = new ContactPerson("Devika", "Chakravarti", "Shivajinagar", "Katni", "Madhya Pradesh",
+				"483501", "9876580098", "devika@example.com");
+
+		ContactPerson person2 = new ContactPerson("Devika", "Chakravarti", "Other Address", "Other City", "Other State",
+				"111111", "0000000000", "duplicate@example.com");
+
+		assertTrue(person1.equals(person2));
+	}
+
+	@Test
+	void givenContact_WhenGetCityAndStateCalled_ShouldReturnExpectedValues() {
+		ContactPerson person = new ContactPerson("Devika", "Chakravarti", "Shivajinagar", "Katni", "Madhya Pradesh",
+				"483501", "9876580098", "devika@example.com");
+
+		assertEquals("Katni", person.getCity());
+		assertEquals("Madhya Pradesh", person.getState());
 	}
 }
